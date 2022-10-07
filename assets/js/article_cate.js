c2d87ca97276c90d82892336081b9610f574166f
wxs <18761886590@163.com>
2022-10-07 19:12:40 +0800

    完成了文章类别

---
+++ assets/js/article_cate.js
@@ -0,0 +1,102 @@
+$(function () {
+  const layer = layui.layer
+  const form = layui.form
+  loadCateList()
+  function loadCateList() {
+    $.ajax({
+      method: 'get',
+      url: '/my/cate/list',
+      success(res) {
+        if (res.code !== 0) {
+          return layer.msg('获取列表失败')
+        }
+        // console.log(res.data);
+        const htmlstr = template('tpl-cate', res)
+        $('tbody').html(htmlstr)
+      }
+    })
+  }
+  let index = null
+  $('#btnAddCate').on('click', function () {
+    index = layer.open({
+      type: 1,
+      area: ['500px', '300px'],
+      title: '添加分类',
+      content: $('#AddCate').html()
+    })
+  })
+  let isEdit = false
+  $('body').on('submit', '#AddForm', function (e) {
+    e.preventDefault()
+    if (isEdit) {
+      $.ajax({
+        method: 'put',
+        url: '/my/cate/info',
+        // data: $(this).serialize(),
+        data: form.val('AddForm'),
+        success(res) {
+          if (res.code !== 0) {
+            return layer.msg('修改分类失败')
+          }
+          console.log(res.data);
+          layer.msg('修改分类成功')
+          loadCateList()
+        }
+      })
+    } else {
+      console.log(form.val('AddForm'));
+      $.ajax({
+        method: 'post',
+        url: '/my/cate/add',
+        // data: $(this).serialize(),
+        data: form.val('AddForm'),
+        success(res) {
+          if (res.code !== 0) {
+            return layer.msg('添加分类失败')
+          }
+          layer.msg('添加分类成功')
+          loadCateList()
+        }
+      })
+    }
+    isEdit = false
+    layer.close(index)
+  })
+  $('tbody').on('click', '.btn-edit', function () {
+    isEdit = true
+    index = layer.open({
+      type: 1,
+      area: ['500px', '300px'],
+      title: '修改分类',
+      content: $('#AddCate').html()
+    })
+
+    const id = $(this).attr('data-id')
+    $.ajax({
+      method: 'get',
+      url: `/my/cate/info?id=${id}`,
+      success(res) {
+        if (res.code !== 0) return layer.msg('回显失败')
+        form.val('AddForm', res.data)
+        // console.log(res.data);
+      }
+    })
+  })
+  $('tbody').on('click', '.btn-delete', function () {
+    const re = confirm('确认删除吗')
+    const id = $('.btn-edit').attr('data-id')
+    if (re) {
+      $.ajax({
+        method: 'DELETE',
+        url: `/my/cate/del?id=${id}`,
+        success(res) {
+          if (res.code !== 0) return layer.msg('删除失败')
+          layer.msg('删除成功')
+          loadCateList()
+
+        }
+      })
+    }
+
+  })
+})
--- assets/js/user_avatar.js
+++ assets/js/user_avatar.js
@@ -14,9 +14,10 @@ $(function(){
    // 1.3 创建裁剪区域
    $image.cropper(options)
 
-   $('.layui-btn').on('click',function(){
+   $('#btnPost').on('click',function(){
     // id特殊
-    file.click()
+    // file.click()
+    $('#file').click()
    })
    $('#file').on('change',function(e){
     const files = e.target.files
@@ -29,4 +30,32 @@ $(function(){
    .attr('src', imgUrl)  // 重新设置图片路径
    .cropper(options)        // 重新初始化裁剪区域
    })
+
+   $('#btnConfirm').on('click',function(){
+    var dataURL = $image
+      .cropper('getCroppedCanvas', { // 创建一个 Canvas 画布
+        width: 100,
+        height: 100
+      })
+      .toDataURL('image/png')       // 将 Canvas 画布上的内容，转化为 base64 格式的字符串
+    $.ajax({
+      method: 'PATCH',
+      url: '/my/update/avatar',
+      data: {
+        avatar: dataURL
+      },
+      success(res){
+        if(res.code !==0){
+          console.log(res);
+          return layer.msg('上传头像失败')
+        }
+        layer.msg('上传头像成功')
+      window.parent.getUserinfo()
+      },
+      error(res){
+        console.log(res);
+      }
+    })
+   })
+
 })
